e08c8dcf54d96d637111e7f69343cdf93ada4922
Rudr <rudrsharma98186940@gmail.com>
2024-10-18 12:07:08 +0530

    update the README.md with link to contributing guide

--- index.js
+++ index.js
@@ -2,6 +2,25 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => res.send("Express on Vercel"));
+app.get('/home' , (req , res) => res.send('this is home'))
+app.get('/users' , (req ,res) => {
+    const user = [
+        {name : 'rudr'} , 
+        {name : 'sahil'} , 
+        {name : 'nikita'} , 
+        {name : 'java'} 
+    ]
+
+    res.send(
+        `<div>
+            ${
+                user.map((ele) => {
+                    return <h1> ${ele.name} </h1>
+                }) 
+            }
+        </div>`
+    )
+})
 
 app.listen(3000, () => console.log("Server ready on port 3000."));
 
